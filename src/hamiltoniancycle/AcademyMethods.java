51489b26d02234a2effdf9ba57ef7a2a23669dd5
Reinaldo <reinaldosmoraes@hotmail.com>
2019-11-28 17:46:37 -0300

    Complete and conex graph test

--- src/hamiltoniancycle/AcademyMethods.java
+++ src/hamiltoniancycle/AcademyMethods.java
@@ -26,9 +26,25 @@ public class AcademyMethods {
                 {1,0,0,0,0,1,0,1},// 6
                 {0,1,0,0,0,0,1,0} // 7
         };
+
+//        Complete graph example
+//        adjcentMatrix = new int[][]{
+//                {0,1,1,1},
+//                {1,0,1,1},
+//                {1,1,0,1},
+//                {1,1,1,0}
+//        };
+
+        //Disconex graph exmaple
+//        adjcentMatrix = new int[][]{
+//                {0,1,0,1},
+//                {1,0,0,1},
+//                {0,0,0,0},
+//                {1,1,0,0}
+//        };
     }
 
-    private void countVerticesDegrees(){
+    private void countVerticesDegrees() {
         for (int i = 0; i < numberOfVertices; i++){
             int count = 0;
             for (int j = 0; j < numberOfVertices ; j++){
@@ -38,7 +54,7 @@ public class AcademyMethods {
         }
     }
 
-    private boolean oreMethod(){
+    private boolean oreMethod() {
         if(numberOfVertices < 3) return false;
         for (int i = 0; i < numberOfVertices ; i++){
             for (int j = 0; j < numberOfVertices ; j++) {
@@ -48,7 +64,7 @@ public class AcademyMethods {
         return true;
     }
 
-    private boolean diracMethod(){
+    private boolean diracMethod() {
         if(numberOfVertices < 3) return false;
         double half = numberOfVertices / 2.0;
         for (Integer i: verticesDegree) {
@@ -58,10 +74,37 @@ public class AcademyMethods {
         return true;
     }
 
+    private boolean completeGraph() {
+        for (int i = 0; i < numberOfVertices ; i++){
+            for (int j = 0; j < numberOfVertices ; j++) {
+                if (adjcentMatrix[i][j] == 0 && i != j) return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean conexGraph() {
+        boolean verticeHasEdge;
+        boolean isConex = true;
+        for (int i = 0; i < numberOfVertices ; i++){
+            verticeHasEdge = false;
+            for (int j = 0; j < numberOfVertices ; j++) {
+                if (adjcentMatrix[i][j] > 0 && i != j) {
+                    verticeHasEdge = true;
+                    break;
+                }
+            }
+            isConex = isConex && verticeHasEdge;
+        }
+        return isConex;
+    }
+
     public static void main(String[] args) {
         AcademyMethods academyMethods = new AcademyMethods();
         System.out.println("Ore`s Method: " + academyMethods.oreMethod());
         System.out.println("Dirac`s Method: " + academyMethods.diracMethod());
+        System.out.println("Complete Graph: " + academyMethods.completeGraph());
+        System.out.println("Conex Graph: " + academyMethods.conexGraph());
     }
 
 }
