fad970c467c993809e3caa3b303340f19ad3a59c
João Victor <joaov.d.oliveira.araujo@gmail.com>
2019-12-01 17:59:08 -0300

    Biconnected Graph Verification

--- src/hamiltoniancycle/AcademyMethods.java
+++ src/hamiltoniancycle/AcademyMethods.java
@@ -1,15 +1,23 @@
 package hamiltoniancycle;
 
+import static java.lang.Integer.min;
+
 public class AcademyMethods {
     private static int[][] adjcentMatrix;
     private static int[] circuit, verticesDegree;
-    private static int numberOfVertices;
+    private static int numberOfVertices, time;
+    private boolean[] vis;
+    private int[] discoveredNodes, lowerNodes, parents;
 
     private AcademyMethods() {
         initializeAdjcentMatrix();
         numberOfVertices = adjcentMatrix.length;
         circuit = new int[numberOfVertices];
         verticesDegree = new int[numberOfVertices];
+        vis = new boolean[numberOfVertices];
+        discoveredNodes = new int[numberOfVertices];
+        lowerNodes = new int[numberOfVertices];
+        parents = new int[numberOfVertices];
         circuit[0] = 0;
         countVerticesDegrees();
     }
@@ -99,12 +107,45 @@ public class AcademyMethods {
         return isConex;
     }
 
+    public boolean findArticulationPointMethod(int start){
+        int dfsChild = 0;
+        time = 0;
+        vis[start] = true;
+        discoveredNodes[start] = lowerNodes[start] = time++;
+
+        for(int v = 0; v < numberOfVertices; v++) {
+            boolean check = (adjcentMatrix[start][v] == 1)?true:false;
+            if(check){
+                if(!vis[v]) {
+                    dfsChild++; parents[v] = start;
+                    if(findArticulationPointMethod(v))  return true;
+                    lowerNodes[start] = min(lowerNodes[start], lowerNodes[v]);
+                    if(parents[start] == -1 && dfsChild > 1) return true;
+                } else if(v != parents[start])   lowerNodes[start] = min(lowerNodes[start], discoveredNodes[v]);
+            }
+        }
+        return false;
+    }
+
+    public boolean biconectedMethod(){
+
+        for(int i = 0; i < numberOfVertices; i++) {
+            vis[i] = false; parents[i] = -1;
+        }
+
+        if(findArticulationPointMethod(0))  return false;  //No articulation Point
+        for(int i = 0; i < numberOfVertices; i++)   if (!vis[i]) return false;    //Exists nodes not visited
+
+        return true;
+    }
+
     public static void main(String[] args) {
         AcademyMethods academyMethods = new AcademyMethods();
+        System.out.println("Conex Graph: " + academyMethods.conexGraph());
+        System.out.println("Biconnected Graph: " + academyMethods.biconectedMethod());
         System.out.println("Ore`s Method: " + academyMethods.oreMethod());
         System.out.println("Dirac`s Method: " + academyMethods.diracMethod());
         System.out.println("Complete Graph: " + academyMethods.completeGraph());
-        System.out.println("Conex Graph: " + academyMethods.conexGraph());
     }
 
 }
